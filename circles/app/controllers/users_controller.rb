37991199e666a72bc753c7f25d06f68ebe120703
dotyng_hh <Dotyng_hh@163.com>
2016-05-23 14:00:33 +0800

    drop bolg

--- circles/app/controllers/users_controller.rb
+++ circles/app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :auth_user, only: [:index]
   def new
     @user = User.new
   end
-
+  def index
+    @users = User.page(params[:page] || 1).per_page(params[:per_page] || 10).order("id desc")
+  end
   def create
     @user = User.new(params.require(:user).permit(:username, :password))
     if @user.save
@@ -10,6 +13,13 @@ class UsersController < ApplicationController
       redirect_to new_session_path
     else
       render action: :new
+    end
+  end
+private
+  def auth_user
+    unless session[:user_id]
+      flash[:notice] =  "请登录"
+      redirect_to new_session_path
     end
   end
 end
