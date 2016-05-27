f809a01329c6cc26fe9f9f92bed7d0e9169d135d
dotyng_hh <Dotyng_hh@163.com>
2016-05-27 11:33:22 +0800

    hh

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,12 @@ class ArticlesController < ApplicationController
   def index
 
   end
-  
   def search
     @articles = Articles.page(params[:page] || 1).per_page(params[:per_page] || 10 )
     .order("id desc").where(["article like ?", "%#{params[:article]}%")
     render index_welcome_path
+  end
+  def create
+    
   end
 end
